532e033f65ba2d71e942a339aae39977f17790eb
unknown <mari.bratkouskaya@gmail.com>
2022-11-04 16:00:42 +0400

    stepic.org 'Интерактивный тренажер по SQL'

--- SQL_courses.sql
+++ SQL_courses.sql
@@ -509,11 +509,342 @@ UPDATE fine,
     --в таблицу fine занести дату оплаты соответствующего штрафа из таблицы payment; 
     --уменьшить начисленный штраф в таблице fine в два раза  (только для тех штрафов, информация о которых занесена в таблицу payment) , если оплата произведена не позднее 20 дней со дня нарушения.
 
+UPDATE fine as f,
+       payment as p
+SET f.date_payment = p.date_payment,
+    f.sum_fine = if(DATEDIFF(p.date_payment, p.date_violation)<=20, f.sum_fine/2, f.sum_fine)
+WHERE (f.name, f.number_plate, f.violation) = (p.name, p.number_plate, p.violation) 
+AND f.date_payment IS NULL;
+SELECT * FROM fine;							 
 							 
+--Создать новую таблицу back_payment, куда внести информацию о неоплаченных штрафах (Фамилию и инициалы водителя, номер машины, нарушение, сумму штрафа  и  дату нарушения) из таблицы fine							 
 							 
+CREATE TABLE back_payment
+SELECT name,
+       number_plate,
+       violation,
+       sum_fine,
+       date_violation
+FROM fine
+WHERE date_payment IS NULL;
 							 
+--Удалить из таблицы fine информацию о нарушениях, совершенных раньше 1 февраля 2020 года. 
 							 
+DELETE FROM fine
+WHERE date_violation<'2020-02-01';
 							 
+--Создать таблицу author 
+				
+CREATE TABLE author(
+    author_id INT PRIMARY KEY AUTO_INCREMENT,
+    name_author VARCHAR(50)
+    );
 							 
+--Заполнить таблицу author. В нее включить следующих авторов:
+
+    --Булгаков М.А.
+    --Достоевский Ф.М.
+    --Есенин С.А.
+    --Пастернак Б.Л.
+
+INSERT INTO author (name_author)
+VALUES ('Булгаков М.А.'),
+        ('Достоевский Ф.М.'),
+        ('Есенин С.А.'),
+        ('Пастернак Б.Л.');
+					 
+--	Перепишите запрос на создание таблицы book , чтобы ее структура соответствовала структуре, показанной на логической схеме (таблица genre уже создана, порядок следования столбцов - как на логической схеме в таблице book, genre_id  - внешний ключ) . Для genre_id ограничение о недопустимости пустых значений не задавать. В качестве главной таблицы для описания поля  genre_idиспользовать таблицу genre 
 							 
+CREATE TABLE book (
+    book_id INT PRIMARY KEY AUTO_INCREMENT, 
+    title VARCHAR(50), 
+    author_id INT NOT NULL,
+    genre_id INT,
+    price DECIMAL(8,2), 
+    amount INT, 
+    FOREIGN KEY (author_id)  REFERENCES author (author_id),
+    FOREIGN KEY (genre_id) REFERENCES genre (genre_id)
+);
 							 
+--Создать таблицу book той же структуры, что и на предыдущем шаге. Будем считать, что при удалении автора из таблицы author, должны удаляться все записи о книгах из таблицы book, написанные этим автором. А при удалении жанра из таблицы genre для соответствующей записи book установить значение Null в столбце genre_id. 
+							 
+CREATE TABLE book (
+    book_id INT PRIMARY KEY AUTO_INCREMENT, 
+    title VARCHAR(50), 
+    author_id INT NOT NULL,
+    genre_id INT,
+    price DECIMAL(8,2), 
+    amount INT, 
+    FOREIGN KEY (author_id)  REFERENCES author (author_id) ON DELETE CASCADE,
+    FOREIGN KEY (genre_id) REFERENCES genre (genre_id) ON DELETE SET NULL);
+							 
+--Добавьте три последние записи (с ключевыми значениями 6, 7, 8) в таблицу book, первые 5 записей уже добавлены:
+
+INSERT INTO book (title, author_id, genre_id, price, amount)
+VALUES ('Стихотворения и поэмы', 3, 2, 650.00, 15),
+        ('Черный человек', 3, 2, 570.20, 6),
+        ('Лирика', 4, 2, 518.99, 2);
+							 
+--Вывести название, жанр и цену тех книг, количество которых больше 8, в отсортированном по убыванию цены виде.							 
+							 
+SELECT title, name_genre, price
+FROM 
+    genre INNER JOIN book
+    ON genre.genre_id = book.genre_id
+WHERE amount>8
+ORDER BY price DESC;
+							 
+--Вывести все жанры, которые не представлены в книгах на складе.							 
+							 
+SELECT name_genre
+FROM genre
+LEFT JOIN book
+ON genre.genre_id = book.genre_id
+WHERE book.genre_id is null;
+							 
+--Есть список городов, хранящийся в таблице city
+--Необходимо в каждом городе провести выставку книг каждого автора в течение 2020 года. Дату проведения выставки выбрать случайным образом. Создать запрос, который выведет город, автора и дату проведения выставки. Последний столбец назвать Дата. Информацию вывести, отсортировав сначала в алфавитном порядке по названиям городов, а потом по убыванию дат проведения выставок.
+							 
+SELECT name_city, 
+       name_author,
+       (DATE_ADD('2020-01-01', INTERVAL FLOOR(RAND() * 365) DAY)) as Дата
+FROM city,
+     author
+ORDER BY name_city,
+         Дата DESC;
+				 
+--Вывести информацию о книгах (жанр, книга, автор), относящихся к жанру, включающему слово «роман» в отсортированном по названиям книг виде.
+				 
+SELECT name_genre,
+       title,
+       name_author
+FROM genre
+INNER JOIN book
+ON genre.genre_id=book.genre_id
+INNER JOIN author
+ON book.author_id=author.author_id
+WHERE name_genre LIKE 'Роман'
+ORDER BY title;
+				 
+--Посчитать количество экземпляров  книг каждого автора из таблицы author.  Вывести тех авторов,  количество книг которых меньше 10, в отсортированном по возрастанию количества виде. Последний столбец назвать Количество.				 
+				 
+SELECT name_author,
+       SUM(amount) AS Количество
+FROM author
+LEFT JOIN book
+ON author.author_id=book.author_id 
+GROUP BY name_author
+HAVING Количество < 10 OR Количество is null
+ORDER BY Количество;
+				 
+--Вывести в алфавитном порядке всех авторов, которые пишут только в одном жанре. Поскольку у нас в таблицах так занесены данные, что у каждого автора книги только в одном жанре,  для этого запроса внесем изменения в таблицу book. Пусть у нас  книга Есенина «Черный человек» относится к жанру «Роман», а книга Булгакова «Белая гвардия» к «Приключениям» (эти изменения в таблицы уже внесены).				 
+				 
+SELECT name_author
+     FROM author INNER JOIN   
+           (SELECT author_id, COUNT(genre_id) AS genre_count
+              FROM (SELECT DISTINCT author_id, genre_id FROM book) q1
+              GROUP BY author_id
+              HAVING genre_count = 1) q2
+        on author.author_id = q2.author_id;
+				 
+--Вывести информацию о книгах (название книги, фамилию и инициалы автора, название жанра, цену и количество экземпляров книги), написанных в самых популярных жанрах, в отсортированном в алфавитном порядке по названию книг виде. Самым популярным считать жанр, общее количество экземпляров книг которого на складе максимально.				 
+							 
+SELECT  title,
+        name_author, 
+        name_genre,
+        price,
+        amount
+FROM 
+    author 
+    INNER JOIN book ON author.author_id = book.author_id
+    INNER JOIN genre ON  book.genre_id = genre.genre_id
+WHERE genre.genre_id IN
+         (
+          SELECT query_in_1.genre_id
+          FROM 
+              (
+                SELECT genre_id, SUM(amount) AS sum_amount
+                FROM book
+                GROUP BY genre_id
+               )query_in_1
+          INNER JOIN 
+              ( 
+                SELECT genre_id, SUM(amount) AS sum_amount
+                FROM book
+                GROUP BY genre_id
+                ORDER BY sum_amount DESC
+                LIMIT 1
+               ) query_in_2
+          ON query_in_1.sum_amount= query_in_2.sum_amount
+         )
+ORDER BY title;
+				 
+--Если в таблицах supply  и book есть одинаковые книги, которые имеют равную цену,  вывести их название и автора, а также посчитать общее количество экземпляров книг в таблицах supply и book,  столбцы назвать Название, Автор  и Количество.
+				 
+SELECT title AS Название,
+       name_author AS Автор,
+       SUM(supply.amount + book.amount) AS Количество
+FROM supply 
+INNER JOIN book
+USING (price, title)
+INNER JOIN author
+ON author.name_author = supply.author
+GROUP BY author.name_author, book.title;
+				 
+--Для книг, которые уже есть на складе (в таблице book), но по другой цене, чем в поставке (supply),  необходимо в таблице book увеличить количество на значение, указанное в поставке,  и пересчитать цену. А в таблице  supply обнулить количество этих книг. 
+				 
+UPDATE book 
+     INNER JOIN author ON author.author_id = book.author_id
+     INNER JOIN supply ON book.title = supply.title 
+                         and supply.author = author.name_author
+SET book.price = (book.price*book.amount + supply.price*supply.amount)/(book.amount+supply.amount), 
+    book.amount = book.amount + supply.amount,
+    supply.amount = 0 
+WHERE supply.price <> book.price;
+
+SELECT * FROM book;
+SELECT * FROM supply;				 
+				 
+--Включить новых авторов в таблицу author с помощью запроса на добавление, а затем вывести все данные из таблицы author.  Новыми считаются авторы, которые есть в таблице supply, но нет в таблице author.				 
+				 
+INSERT INTO author (name_author)
+SELECT supply.author
+FROM author
+RIGHT JOIN supply 
+ON author.name_author = supply.author
+WHERE name_author IS Null;
+				 
+--Добавить новые книги из таблицы supply в таблицу book на основе сформированного выше запроса. Затем вывести для просмотра таблицу book.
+				 
+INSERT INTO book (title, author_id, price, amount)
+SELECT title, author_id, price, amount
+FROM 
+    author 
+    INNER JOIN supply ON author.name_author = supply.author
+WHERE amount <> 0;
+SELECT * FROM book;
+				 
+--Занести для книги «Стихотворения и поэмы» Лермонтова жанр «Поэзия», а для книги «Остров сокровищ» Стивенсона - «Приключения». (Использовать два запроса).				 
+				 
+UPDATE book
+SET genre_id = 
+      (
+       SELECT genre_id 
+       FROM genre 
+       WHERE name_genre = 'Поэзия'
+      )
+WHERE title LIKE 'Стихотворения и поэмы';
+
+UPDATE book
+SET genre_id = 
+      (
+       SELECT genre_id 
+       FROM genre 
+       WHERE name_genre = 'Приключения'
+      )
+WHERE title LIKE 'Остров сокровищ';
+
+SELECT * FROM book;
+
+--Удалить всех авторов и все их книги, общее количество книг которых меньше 20.				 
+				 
+DELETE FROM author
+WHERE author_id IN(SELECT author_id  
+    FROM book
+    GROUP BY author_id
+    HAVING SUM(amount)<20);
+SELECT * FROM author;
+SELECT * FROM book;				 
+				 
+--Удалить все жанры, к которым относится меньше 4-х книг. В таблице book для этих жанров установить значение Null.							 
+							 
+DELETE FROM genre
+WHERE genre_id IN (
+    SELECT genre_id
+    FROM book
+    GROUP BY genre_id
+    HAVING count(book_id)<4);
+
+SELECT * FROM genre;
+SELECT * FROM book;				 
+				 
+--Удалить всех авторов, которые пишут в жанре "Поэзия". Из таблицы book удалить все книги этих авторов. В запросе для отбора авторов использовать полное название жанра, а не его id.				 
+				 
+DELETE FROM author
+USING author
+INNER JOIN book USING(author_id)
+INNER JOIN genre USING (genre_id)
+WHERE genre.name_genre LIKE 'Поэзия';
+
+SELECT * FROM author;
+SELECT * FROM book;
+				 
+--Вывести все заказы Баранова Павла (id заказа, какие книги, по какой цене и в каком количестве он заказал) в отсортированном по номеру заказа и названиям книг виде.				 
+				 
+SELECT DISTINCT buy.buy_id,
+                title,
+                price,
+                buy_book.amount
+FROM 
+    client 
+    INNER JOIN buy ON client.client_id = buy.client_id
+    INNER JOIN buy_book ON buy_book.buy_id = buy.buy_id
+    INNER JOIN book ON buy_book.book_id=book.book_id
+WHERE name_client LIKE 'Баранов Павел'
+ORDER BY buy_id, title;				 
+				 
+--Посчитать, сколько раз была заказана каждая книга, для книги вывести ее автора (нужно посчитать, в каком количестве заказов фигурирует каждая книга).  Вывести фамилию и инициалы автора, название книги, последний столбец назвать Количество. Результат отсортировать сначала  по фамилиям авторов, а потом по названиям книг.
+				 
+SELECT name_author, 
+       title, 
+       count(buy_book.amount) AS Количество
+FROM author
+INNER JOIN book USING(author_id)
+LEFT JOIN buy_book USING(book_id)
+GROUP BY book.title, name_author
+ORDER BY name_author, title;
+				 
+--Вывести города, в которых живут клиенты, оформлявшие заказы в интернет-магазине. Указать количество заказов в каждый город, этот столбец назвать Количество. Информацию вывести по убыванию количества заказов, а затем в алфавитном порядке по названию городов.
+				 
+SELECT name_city,
+       count(buy.client_id) AS Количество
+FROM buy
+INNER JOIN client USING (client_id)
+INNER JOIN city USING (city_id)
+GROUP BY name_city
+ORDER BY Количество DESC;
+				 
+--Вывести номера всех оплаченных заказов и даты, когда они были оплачены.				 
+				 
+SELECT buy_id,
+       date_step_end
+FROM step
+INNER JOIN buy_step USING (step_id)
+WHERE name_step LIKE 'Оплата' AND date_step_end IS NOT NULL;
+				 
+--Вывести информацию о каждом заказе: его номер, кто его сформировал (фамилия пользователя) и его стоимость (сумма произведений количества заказанных книг и их цены), в отсортированном по номеру заказа виде. Последний столбец назвать Стоимость.				 
+				 
+SELECT s.buy_id, 
+       name_client, 
+       SUM(Стоимость) AS Стоимость
+FROM (
+            SELECT buy_book.buy_id, book.price * buy_book.amount AS Стоимость
+            FROM buy_book
+            INNER JOIN book ON buy_book.book_id = book.book_id
+      ) AS s
+            INNER JOIN buy ON s.buy_id = buy.buy_id
+            INNER JOIN client ON buy.client_id = client.client_id
+GROUP BY buy_id
+ORDER BY s.buy_id;		
+				 
+--Вывести номера заказов (buy_id) и названия этапов,  на которых они в данный момент находятся. Если заказ доставлен –  информацию о нем не выводить. Информацию отсортировать по возрастанию buy_id.
+				 
+SELECT buy_id,
+       name_step
+FROM buy_step
+INNER JOIN step USING (step_id)
+WHERE date_step_beg IS NOT NULL AND date_step_end IS NULL
+ORDER BY buy_id;
+				 
+--В таблице city для каждого города указано количество дней, за которые заказ может быть доставлен в этот город (рассматривается только этап Транспортировка). Для тех заказов, которые прошли этап транспортировки, вывести количество дней за которое заказ реально доставлен в город. А также, если заказ доставлен с опозданием, указать количество дней задержки, в противном случае вывести 0. В результат включить номер заказа (buy_id), а также вычисляемые столбцы Количество_дней и Опоздание. Информацию вывести в отсортированном по номеру заказа виде.
+				 
